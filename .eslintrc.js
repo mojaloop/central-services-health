9965e403a17bfe42a1741c79b5d53435349f1d10
Miguel de Barros <miguel@debarros.me>
2022-05-19 15:33:11 +0200

    feat(mojaloop/#2092): upgrade nodeJS version for core services (#250)
    feat(mojaloop/#2092): upgrade nodeJS version for core services - https://github.com/mojaloop/project/issues/2092
    - removed unused files
    - removed unused dependencies
    - re-aligned package.json
    - added replace dependency
    - added IGNORE and test/result patterns to gitignore
    - re-aligned lint standards config
    - re-aligned tsconfig
    
    Notes:
    - npm-audit-resolver v3.0.0-7 is a candidate release to resolve compatibility with npm v7+ as described in https://github.com/naugtur/npm-audit-resolver/issues/34. This will need to be addressed going forward as npm run audit:resolve (i.e. resolve-audit) is currently not functioning. As a work-around, we need to manually run the following command npm audit fix. The npm run audit:check (i.e. check-audit) still works as expected.
    
    BREAKING CHANGE: major version bump for node v16 LTS support, and re-structuring of project directories to align to core Mojaloop repositories!

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:prettier/recommended'
+    // Enforces ES6+ import/export syntax
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -10,12 +14,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    indent: [2, 2],
-    quotes: [2, 'single'],
-    'linebreak-style': [2, 'unix'],
-    semi: [2, 'never']
+    '@typescript-eslint/no-var-requires': 'off'
   },
   overrides: [
     {
